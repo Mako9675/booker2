5962cc41107ccbac3222a2d10fd02d9b5bed28d3
Mako9675 <Mako9675@users.noreply.github.com>
2023-01-21 09:40:25 +0000

    [finish] userコントローラ/投稿の削除と確認ポップアップ実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'homes/top'
   
   root to: 'homes#top'
-  resources :books, only: [:create, :index, :show]
+  resources :books, only: [:create, :index, :show, :destroy]
   devise_for :users
+  
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

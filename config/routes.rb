0784f9b6a28503a3576cfc6c1fbc2dfac844c0c3
EC2 Default User <ec2-user@ip-172-31-35-105.ap-northeast-1.compute.internal>
2023-01-21 07:54:39 +0000

    [add] index/showにユーザー名でリンク紐づけ

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'homes/top'
   
   root to: 'homes#top'
-  resources :books,only: [:new, :index, :show]
+  resources :books, only: [:create, :index, :show]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

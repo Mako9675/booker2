ec1f2f814d6aabe2bfa92beadd89bd847a70daa7
Mako9675 <Mako9675@users.noreply.github.com>
2023-01-22 08:49:22 +0000

    [add] ログアウト時Topへ/フッターに©作成/ヘッダーにHMのリンク作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,35 @@
 class UsersController < ApplicationController
+  before_action :ensure_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!
   
   def index
     @user = User.all
   end
   
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by(id:current_user.id)
     @books = @user.books
   end
+  
+  
 
   def edit
     @user = User.find(params[:id])
   end
   
   def update
-    @user.image.attach(params[:user][:image]) if @user.image.blank?
-    if @user.update(user_params)
-      flash[:notice] = "success"
-      redirect_to user_path
-    else
-      render "edit", atatus: :unprocessable_entity
-    end
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(current_user.id)
   end
   
   private
   def user_params
     param.require(:book).permit(:name, :introduction, :image)
+  end
+  def ensure_user
+    @users = current_user.users
+    @user = @users.find_by(id: params[:id])
+    redirect_to user_path unless @user
   end
 end

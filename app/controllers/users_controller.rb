530dee6ce8b28295cdcb0ba0b57cfc6102b08014
Mako9675 <Mako9675@users.noreply.github.com>
2023-01-22 10:49:54 +0000

    [finish] 全ページのリンク/リダイレクト先,update/delete実装完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,15 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id:current_user.id)
     @books = @user.books
+    @book = Book.new
   end
   
-  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
 
   def edit
     @user = User.find(params[:id])
